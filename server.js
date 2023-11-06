15e519919b1f2e98fb600af6e2ae31eb4a60545d
Silverfoot42 <elliottgc@gmail.com>
2023-11-06 01:25:31 -0500

    created routes folder and correctly pathed it

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const htmlRoute = require('./routes/htmlRoute.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', htmlRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
